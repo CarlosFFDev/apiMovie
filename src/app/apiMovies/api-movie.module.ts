b5f863792b58fe9b0a2b3531d373e0b977bda51d
CarlosFFDev <jcarlosf.dev@gmail.com>
2024-04-06 13:49:38 -0600

    chore(apiMovie): project structuring

---
+++ src/app/apiMovies/api-movie.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from './pages/search/search.component';
+
+
+
+@NgModule({
+  declarations: [
+    SearchComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ApiMovieModule { }
